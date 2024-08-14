b3115b02690465bdc01c4fb447fdbaac52fa8ea8
mobin9229 <119866019+mobin9229@users.noreply.github.com>
2024-08-14 16:28:43 +0330

    models.py

--- hotel_reservation/carts/models.py
+++ hotel_reservation/carts/models.py
@@ -1,11 +1,10 @@
-# carts/models.py
 from django.db import models
 from django.conf import settings
-from product.models import Product  # فرض می‌کنیم مدل Product در اپ product قرار دارد
+from product.models import Product  
 
 class Cart(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    # دیگر فیلدها در صورت نیاز
+
 
 class CartItem(models.Model):
     cart = models.ForeignKey(Cart, related_name='items', on_delete=models.CASCADE)
